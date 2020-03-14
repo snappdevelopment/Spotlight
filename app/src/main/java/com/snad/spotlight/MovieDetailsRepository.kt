f945cab36799d54c3219131622a3ad011fec8d27
snappdevelopment <sebastian.neubauer@hof-university.de>
2020-03-14 16:44:09 +0100

    add movie details fragment files

---
+++ app/src/main/java/com/snad/spotlight/MovieDetailsRepository.kt
@@ -0,0 +1,5 @@
+package com.snad.spotlight
+
+class MovieDetailsRepository {
+
+}
---
+++ app/src/main/java/com/snad/spotlight/ui/movieDetails/MovieDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.snad.spotlight.ui.movieDetails
+
+import androidx.lifecycle.ViewModel
+
+class MovieDetailsViewModel: ViewModel() {
+
+}
