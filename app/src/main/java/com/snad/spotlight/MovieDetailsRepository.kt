937b93ca96bfd88e2be4d265919ef31579f04238
snappdevelopment <sebastian.neubauer@hof-university.de>
2020-04-06 10:45:08 +0200

    implement updateMovieData

--- app/src/main/java/com/snad/spotlight/MovieDetailsRepository.kt
+++ app/src/main/java/com/snad/spotlight/MovieDetailsRepository.kt
@@ -1,5 +1,6 @@
 package com.snad.spotlight
 
+import android.util.Log
 import com.snad.spotlight.network.MovieApi
 import com.snad.spotlight.network.MovieApiResult
 import com.snad.spotlight.persistence.LibraryDb
@@ -8,13 +9,13 @@ import com.snad.spotlight.persistence.models.LibraryMovie
 import com.snad.spotlight.persistence.toLibraryMovie
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.util.*
 
 class MovieDetailsRepository(
     private val libraryDb: LibraryDb,
     private val movieApi: MovieApi
 ) {
     suspend fun loadMovie(id: Int): Flow<MovieDetailsResult> {
-        //Todo: load changes from Api and write to db
         val result = libraryDb.getMovieById(id)
         return result.map { libraryDbResult ->
             when(libraryDbResult) {
@@ -35,6 +36,27 @@ class MovieDetailsRepository(
 
     suspend fun updateMovie(movie: LibraryMovie) {
         libraryDb.updateMovie(movie)
+    }
+
+    suspend fun updateMovieData(movie: LibraryMovie) {
+        if(movie.updated_at != null) {
+            var daysDifference = Calendar.getInstance().timeInMillis - movie.updated_at.timeInMillis
+            daysDifference = daysDifference / (1000 * 60 * 60 * 24)
+
+            if(daysDifference >= 2) {
+                val result = movieApi.loadMovie(movie.id)
+                when(result) {
+                    is MovieApiResult.Success -> {
+                        val updatedMovie = result.movie.toLibraryMovie(
+                            addedAt = movie.added_at,
+                            updatedAt = Calendar.getInstance(),
+                            hasBeenWatched = movie.has_been_watched
+                        )
+                        libraryDb.updateMovie(updatedMovie)
+                    }
+                }
+            }
+        }
     }
 
     private suspend fun loadMovieFromApi(id: Int): MovieDetailsResult {
