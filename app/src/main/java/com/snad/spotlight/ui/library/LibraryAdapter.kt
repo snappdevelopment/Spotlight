77d190e4a855e361f006c3096ed56b28869b787f
snappdevelopment <sebastian.neubauer@hof-university.de>
2020-09-05 12:07:33 +0200

    use bind() method in view holder

--- app/src/main/java/com/snad/spotlight/ui/library/LibraryAdapter.kt
+++ app/src/main/java/com/snad/spotlight/ui/library/LibraryAdapter.kt
@@ -20,62 +20,52 @@ class LibraryAdapter(
 ): ListAdapter<LibraryMovie, LibraryAdapter.LibraryViewHolder>(LibraryMovieDiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LibraryViewHolder {
-        val viewBinding = RecyclerviewItemLibraryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return LibraryViewHolder(viewBinding)
+        val binding = RecyclerviewItemLibraryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return LibraryViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: LibraryViewHolder, position: Int) {
         val item = getItem(position)
-
-        holder.coverCardView.transitionName = "cover${item.id}"
-
-        holder.movieCard.setOnLongClickListener {
-            longClickListener(item)
-            true
-        }
-        holder.movieCard.setOnClickListener {
-            clickListener(
-                item.id,
-                holder.coverCardView
-            )
-        }
-        holder.hasBeenWatchedFAB.setOnClickListener {
-            val watchedItem = item.copy(has_been_watched = !item.has_been_watched)
-            watchedClickListener(watchedItem)
-        }
-        holder.hasBeenWatchedFAB.isSelected = item.has_been_watched
-        val picasso = Picasso.get()
-//        picasso.setIndicatorsEnabled(true)
-        picasso.load("https://image.tmdb.org/t/p/w154${item.poster_path}")
-            .resize(154, 231)
-            .centerCrop()
-            .placeholder(R.drawable.cover_image_placeholder)
-            .error(R.drawable.cover_image_error)
-            .transform(RoundedCornersTransformation(4, 1))
-            .into(holder.coverImageView)
-
-        holder.titleTextView.text = item.title
-        if(item.release_date == "") holder.releaseDateTextView.visibility = View.GONE
-        else holder.releaseDateTextView.text = item.release_date.substring(0, 4)
-        holder.runtimeTextView.text = if(item.runtime != null) "${item.runtime} min" else ""
-        holder.averageVoteTextView.text = item.vote_average.toString()
-        holder.genreTextView.text = item.genres
+        holder.bind(item)
     }
 
     override fun getItemCount(): Int = currentList.size
 
-    class LibraryViewHolder(
-        viewBinding: RecyclerviewItemLibraryBinding
-    ): RecyclerView.ViewHolder(viewBinding.root) {
-        val movieCard = viewBinding.movieCard
-        val coverImageView = viewBinding.coverImageView
-        val coverCardView = viewBinding.coverCardView
-        val titleTextView = viewBinding.titleTextView
-        val releaseDateTextView = viewBinding.releaseDateTextView
-        val runtimeTextView = viewBinding.runtimeTextView
-        val averageVoteTextView = viewBinding.averageVoteTextView
-        val genreTextView = viewBinding.genreTextView
-        val hasBeenWatchedFAB = viewBinding.hasBeenWatchedFAB
+    inner class LibraryViewHolder(
+        private val binding: RecyclerviewItemLibraryBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: LibraryMovie) {
+            binding.coverCardView.transitionName = "cover${item.id}"
+            binding.movieCard.setOnLongClickListener {
+                longClickListener(item)
+                true
+            }
+            binding.movieCard.setOnClickListener {
+                clickListener(item.id, binding.coverCardView)
+            }
+            binding.hasBeenWatchedFAB.setOnClickListener {
+                val watchedItem = item.copy(has_been_watched = !item.has_been_watched)
+                watchedClickListener(watchedItem)
+            }
+            binding.hasBeenWatchedFAB.isSelected = item.has_been_watched
+            val picasso = Picasso.get()
+//        picasso.setIndicatorsEnabled(true)
+            picasso.load("https://image.tmdb.org/t/p/w154${item.poster_path}")
+                .resize(154, 231)
+                .centerCrop()
+                .placeholder(R.drawable.cover_image_placeholder)
+                .error(R.drawable.cover_image_error)
+                .transform(RoundedCornersTransformation(4, 1))
+                .into(binding.coverImageView)
+
+            binding.titleTextView.text = item.title
+            if(item.release_date == "") binding.releaseDateTextView.visibility = View.GONE
+            else binding.releaseDateTextView.text = item.release_date.substring(0, 4)
+            binding.runtimeTextView.text = if(item.runtime != null) "${item.runtime} min" else ""
+            binding.averageVoteTextView.text = item.vote_average.toString()
+            binding.genreTextView.text = item.genres
+        }
     }
 
     object LibraryMovieDiffCallback: DiffUtil.ItemCallback<LibraryMovie>() {
